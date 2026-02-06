89de040ca5ceccca74b7c222274591432e6a4285
samsia251-byte <samsia251@gmail.com>
2026-02-06 20:09:44 +0600

    Add files via upload

---
+++ loop4.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n = get_int("Enter a number: ");
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0) // check if i is even
+        {
+            printf("%i\n", i);
+        }
+    }
+}
